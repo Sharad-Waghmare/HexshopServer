77fdd73cdba9e5ab74840fb92694350fb4a55749
sharad-waghmare <sharadwaghmare93@gmail.com>
2023-12-07 12:56:46 +0530

    first

--- index.js
+++ index.js
@@ -8,45 +8,18 @@ import productsRouter from "./router/products.router";
 import cartRouter from "./router/cart.router";
 import orderRouter from "./router/order.router";
 
-// const stripe = require('stripe')("sk_test_51O6rt0SFEXnUeQdhmLI5gPtdCRDGGS8kl1R9gXsNg7cmY9l9t7PefDVYPtZysdU9HN464un8Ejj1HtvuIQixVjoN0014GWhRc6");
 
 var app = express();
+require("dotenv").config();
 var cors = require('cors');
 
 
-var PORT = process.env.PORT || 8001
+var PORT = process.env.PORT || PORT
 
 var corsOptions = {
   origin:"*",
   optionsSuccessStatus:200,
 }
-
-// checkout api
-// app.post("/api/create-checkout-session",async(req,res)=>{
-//   const {products} = req.body;
-//   // console.log(products)
-
-//   const lineitems = products.map((product)=>({
-//     price_data:{
-//       currency:"inr",
-//       product_data:{
-//         name:product.name
-//       },
-//       unit_amount:product.price *100,
-//     },
-//     quantity:product.quantity
-//   }))
-
-//   const session = await stripe.checkout.sessions.create({
-//     payment_method_types:["card"],
-//     line_items:lineitems,
-//     mode:"payment",
-//     success_url:"http://localhost:3000/success",
-//     cancel_url:"http://localhost:3000/cancel",
-//   });
-
-//   res.json({id:session.id})
-// })
 
 
 app.use(cors(corsOptions))
@@ -57,8 +30,16 @@ app.listen(PORT,()=>{
     console.log("server running on "+ PORT);
 });
 
-mongoose.connect('mongodb://0.0.0.0:27017/projectData')
-  .then(() => console.log('Connected!'));
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: "true",
+  useUnifiedTopology: "true"
+})
+.then(() => {
+  console.log("DB Connetion Successfull");
+})
+.catch((err) => {
+  console.log(err.message);
+});
 
 app.use('/users', userRouter);
 app.use('/categories', categorysRouter);
